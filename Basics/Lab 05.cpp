32fc2a13b6382c748adffac9cb89d574fd7f43c5
patriciom2 <49079309+patriciom2@users.noreply.github.com>
2019-04-10 12:45:14 -0500

    Add files via upload

---
+++ Basics/Lab 05.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	const double cm_per_inch=2.54;
+	int length=1;
+	char unit='a';
+	cout<<"Por favor ingrese una longitud y su unidad (c o i)"<<endl;
+	cin>>length>>unit;
+	
+	switch (unit){
+		case 'i':
+			cout<<length<<"in = "<<cm_per_inch*length<<"cm\n";
+			break;
+		case 'c':
+			cout<<length<<"cm = "<<cm_per_inch/length<<"in\n";
+			break;
+		default:
+			cout<<"Lo siento, no conosco la unidad '"<<unit<<" '"<<endl;
+			break;
+	}
+}
+
