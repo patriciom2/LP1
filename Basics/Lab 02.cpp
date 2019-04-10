272fdc6c7a6ba2a23fad698933c98fd66af26a91
patriciom2 <49079309+patriciom2@users.noreply.github.com>
2019-04-10 15:14:22 -0500

    Add files via upload

---
+++ Basics/Lab 02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int length =20;
+	int width=40;
+	int area=length*width;
+	length=99;
+	{int perimerter=(length+width)*2;
+	}
+	{int perimeter=length*2+width*2;
+	}
+return 0;
+}
