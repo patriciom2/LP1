a4e9aa9d9a91e230772b6f40fd883df9d8698dc1
patriciom2 <49079309+patriciom2@users.noreply.github.com>
2019-04-10 20:48:59 -0500

    Add files via upload

---
+++ Basics/Ejercicio 04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+		char i;
+	    char k;
+	    int g=0;
+	
+	    for(i='a';i<='z';i++){
+		    cout<<i<<"\t"<<g<<endl;
+		    g++;
+	    }
+	    cout<<endl;
+	
+	    for(k='A';k<='Z';k++){
+		    cout<<k<<"\t"<<g<<endl;
+		    g++;
+	    }
+	    cout<<endl;
+
+	    for(int h=0;h<=7;h++){
+		    cout<<h<<endl;
+	    }
+	    cout<<endl;
+}
+
