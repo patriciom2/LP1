a84df0ad428ed7f5c1548c380f086a5f11184264
patriciom2 <49079309+patriciom2@users.noreply.github.com>
2019-04-26 15:59:23 -0500

    Add files via upload

---
+++ Basics/Sapo.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int cont=0;
+  int a;
+  int x;
+  int s;
+	int saltos[a]; 
+  int z=1; 
+  int patron=0;
+  int mt=0; 	
+  cout << "Ingrese la cantidad de saltos que dara el sapo"<<endl;
+	cin>>x;
+	if (x>=2)
+  {
+		for (int y=0; y<x; y++)
+    {
+		cout <<z<< " Salto"<<endl;
+		z++;
+		cin>>saltos[y];
+	}
+	cout <<" Ingrese la cantidad de metros que desea saber sobre los saltos"<<endl;
+	cin>>mt;
+	for (s=0; s<x; s++)
+  {
+		if (saltos[s] < mt)
+		{
+		cont++;
+		}
+	}
+	cout << "La cantidad de saltos son: "<< cont<<endl;
+	}
+	else{
+		cout << "Tienen que ser mayor que un salto; NO SEA GIL"<<endl;
+	}
+
+}
