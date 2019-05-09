07a3e70363929947ac689a09af68adeadaa79425
patriciom2 <49079309+patriciom2@users.noreply.github.com>
2019-05-09 11:30:30 -0500

    Add files via upload

---
+++ Basics 2/Miscellaneous/Ejercicio 2.3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<stdexcept>
+using namespace std;
+
+int main(){
+	int a, b, c;
+	cout<<"Ingrese tres numeros \n"<<endl;
+	cin>>a>>b>>c;
+	if(a==b && a==c){
+		cout<<"Son iguales"<<endl;
+	}
+}
